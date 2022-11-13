31cc159f1d9e4ecd0bc38d0822db4cbc60dd5c07
beyourbabe60 <beyourbabe60@gmail.com>
2022-11-13 13:43:27 +0700

    f

---
+++ spec/600domain-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import 600domainView from '../lib/600domain-view';
+
+describe('600domainView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
